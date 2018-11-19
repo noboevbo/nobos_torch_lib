8642336f52b6a5db598bfe23aa48a889da0b5340
Dennis Ludl <Dennis.Ludl@reutlingen-university.de>
2018-11-19 13:13:44 +0100

    Fixed package name

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
-setup(name='nobos_commons',
+setup(name='nobos_torch_lib',
       version='0.1',
-      description='Nobos Common Packages. Used in multiple projects.',
+      description='Nobos PyTorch Library. Various datasets, models and utils for PyTorch models.',
       author='Dennis Ludl',
       author_email='dennis@noboevbo.de',
       license='MIT',
