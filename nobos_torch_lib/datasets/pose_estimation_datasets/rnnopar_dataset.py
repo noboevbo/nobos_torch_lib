fd383db9e68d0930ba4f5d9ccd224de6724002ae
Dennis Ludl <Dennis.Ludl@reutlingen-university.de>
2019-01-16 10:48:29 +0100

    Fixed naming of uids

--- nobos_torch_lib/datasets/pose_estimation_datasets/rnnopar_dataset.py
+++ nobos_torch_lib/datasets/pose_estimation_datasets/rnnopar_dataset.py
@@ -1,5 +1,3 @@
-from typing import List
-
 from nobos_commons.data_structures.constants.dataset_slit_type import DatasetSplitType
 from pymongo.collection import Collection
 from torch.utils.data import Dataset
@@ -10,20 +8,14 @@ from nobos_torch_lib.datasets.models.rnnopar_ground_truth import RnnOpArGroundTr
 class RnnOpArDataset(Dataset):
     __slots__ = ['db_collection']
 
-    def __init__(self, db_collection: Collection):
+    def __init__(self, db_collection: Collection, dataset_split_type: DatasetSplitType):
         self.db_collection = db_collection
-        self.__length = self.db_collection.find({'dataset_split.split_type': DatasetSplitType.TRAIN.name}).count()
-
-    def __get_entries(self):
-        db_entries = list()
-        entries: List[RnnOpArGroundTruth] = []
-        for entry in db_entries:
-            entries.append(RnnOpArGroundTruth.from_dict(entry))
-        return entries
+        self.dataset_split_type: DatasetSplitType = dataset_split_type
+        self.__length = self.db_collection.find({'dataset_split.split_type': self.dataset_split_type.name}).count()
 
     def __len__(self):
         return self.__length
 
     def __getitem__(self, index):
-        db_entry = self.db_collection.find_one({'uid': 'train_{0}'.format(index)})
+        db_entry = self.db_collection.find_one({'uid': '{0}_{1}'.format(self.dataset_split_type.name, index)})
         return RnnOpArGroundTruth.from_dict(db_entry)
