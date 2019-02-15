524ee5851b4d3e540bfe39b0a6e3381b9ef3c28f
Dennis Ludl <Dennis.Ludl@reutlingen-university.de>
2019-02-15 09:09:34 +0100

    Ehpi Dataset updates

--- nobos_torch_lib/datasets/action_recognition_datasets/ehpi_dataset.py
+++ nobos_torch_lib/datasets/action_recognition_datasets/ehpi_dataset.py
@@ -1,4 +1,3 @@
-import cv2
 import random
 
 import numpy as np
@@ -35,47 +34,15 @@ class EhpiDataset(Dataset):
         X_ = np.array(rows, dtype=np.float32)
         blocks = int(len(X_) / 32)
         X_ = np.array(np.split(X_, blocks))
-        # np.save(X_path + "x_wo_split", X_)
-        # file.close()
-        #
-        # if os.path.exists(X_path+".pkl.npy"):
-        #     X_ = np.load(X_path+".pkl.npy")
-        # else:
-        #     if os.path.exists(X_path + "x_wo_split.npy"):
-        #         X_ = np.load(X_path + "x_wo_split.npy")
-        #     else:
-        #         file = open(X_path, 'r')
-        #         rows = []
-        #         for row in file:
-        #             rows.append(np.asarray(list(map(float, row.split(',')))))
-        #         # X_ = np.array(
-        #         #     [elem for elem in [
-        #         #         row.split(',') for row in file
-        #         #     ]],
-        #         #     dtype=np.float32
-        #         # )
-        #         X_ = np.array(rows, dtype=np.float32)
-        #         np.save(X_path + "x_wo_split", X_)
-        #         file.close()
-        #     blocks = int(len(X_) / 32)
-        #
-        #     X_ = np.array(np.split(X_, blocks))
-        #     np.save(X_path+".pkl", X_)
-        # if self.normalize_by_max:
-        #     X_[:, :, ::2] *= (1 / np.max(X_[:, :, ::2]))
-        #     X_[:, :, 1::2] *= (1/np.max(X_[:, :, 1::2]))
+
+        # Remove joint positions outside of the image
         X_[X_ < 0] = 0
         X_[X_ > 1] = 1
+        # Transpose to SequenceNum, Channel, Width, Height
         X_ = np.transpose(X_, (0, 3, 1, 2))
+        # Set last channel (currently containing the joint probability) to zero
         X_[:, 2, :, :] = 0
-        # for i in range(0, 100):
-        #     action_img = np.transpose(X_[i], (1, 2, 0))
-        #     action_img = cv2.resize(action_img, (action_img.shape[0] * 10, action_img.shape[1] * 10))
-        #     cv2.imshow("ehpi", action_img)
-        #     # action_img = X_[1400+i]
-        #     # action_img = cv2.resize(action_img, (action_img.shape[0] * 10, action_img.shape[1] * 10))
-        #     # cv2.imshow("ehpi2", action_img)
-        #     cv2.waitKey(0)
+
         return X_
 
         # Load the networks outputs
@@ -91,7 +58,10 @@ class EhpiDataset(Dataset):
         file.close()
 
         # for 0-based indexing
-        return y_ - 1
+        if y_.max() > 0:
+            y_ = y_ - 1
+
+        return y_
 
     def __len__(self):
         return self.__length
@@ -101,39 +71,6 @@ class EhpiDataset(Dataset):
         if self.transform:
             sample = self.transform(sample)
         return sample
-
-# class RnnOpArDataset(Dataset):
-#     __slots__ = ['db_collection']
-#
-#     def __init__(self, db_collection: Collection, dataset_split_type: DatasetSplitType):
-#         self.db_collection = db_collection
-#         self.dataset_split_type: DatasetSplitType = dataset_split_type
-#         self.__length = self.db_collection.find({'dataset_split.split_type': self.dataset_split_type.name}).count()
-#         self.action_mapping: Dict[int, str] = {
-#             "jumping": 0,
-#             "jumping_jacks": 1,
-#             "boxing": 2,
-#             "waving_two_hands": 3,
-#             "waving_one_hand": 4,
-#             "clapping_hands": 5
-#         }
-#
-#     def __len__(self):
-#         return self.__length
-#
-#     def __getitem__(self, index):
-#         db_entry = self.db_collection.find_one({'uid': '{0}_{1}'.format(self.dataset_split_type.name, index)})
-#         gt = RnnOpArGroundTruth.from_dict(db_entry)
-#         x = np.zeros((1, 32, 36), dtype=np.float32)
-#         for seq_idx, frame in enumerate(gt.frames):
-#             for joint_num, i in enumerate(range(0, 36, 2)):
-#                 joint = frame.joints[joint_num]
-#                 x[0][seq_idx][i] = joint.x
-#                 x[0][seq_idx][i+1] = joint.y
-#
-#         # shape: batch_size, sequence_length, num_joints
-#
-#         return {"gt": RnnOpArGroundTruth.from_dict(db_entry), "x": x, "y": self.action_mapping[gt.action]}
 
 
 class ScaleEhpi(object):
@@ -159,10 +96,6 @@ class ScaleEhpi(object):
         factor = random.uniform(min_factor, max_factor)
         ehpi_img[0, :, :] = ehpi_img[0, :, :] * factor
         ehpi_img[1, :, :] = ehpi_img[1, :, :] * factor
-        # ehpi_img[:, 0::self.step_size][ehpi_img[:, 0::self.step_size] > self.image_size.width] = 0
-        # ehpi_img[:, 1::self.step_size][ehpi_img[:, 0::self.step_size] == 0] = 0
-        # ehpi_img[:, 1::self.step_size][ehpi_img[:, 1::self.step_size] > self.image_size.height] = 0
-        # ehpi_img[:, 0::self.step_size][ehpi_img[:, 1::self.step_size] == 0] = 0
         sample['x'] = ehpi_img
         return sample
 
@@ -187,6 +120,7 @@ class TranslateEhpi(object):
         sample['x'] = ehpi_img
         return sample
 
+
 class NormalizeEhpi(object):
     def __init__(self, image_size: ImageSize):
         if image_size.width > image_size.height:
@@ -206,5 +140,38 @@ class NormalizeEhpi(object):
         max_factor_y = self.image_size.width / np.max(ehpi_img[1, :, :])
         ehpi_img[0, :, :] = ehpi_img[0, :, :] * max_factor_x
         ehpi_img[1, :, :] = ehpi_img[1, :, :] * max_factor_y
+        sample['x'] = ehpi_img
+        return sample
+
+
+class FlipEhpi(object):
+    def __init__(self, with_scores: bool = True):
+        self.step_size = 3 if with_scores else 2
+        self.left_indexes = [5, 6, 7, 11, 12, 13, 15, 17]
+        self.right_indexes = [2, 3, 4, 8, 9, 10, 14, 16]
+
+    def __call__(self, sample):
+        if bool(random.getrandbits(1)):
+            return sample
+        ehpi_img = sample['x']
+        curr_min_x = np.min(ehpi_img[0, :, :])
+        # curr_min_y = np.min(ehpi_img[1, :, :])
+        curr_max_x = np.max(ehpi_img[0, :, :])
+        # curr_max_y = np.max(ehpi_img[1, :, :])
+        # reflect_y = (curr_max_y + curr_min_y) / 2
+        reflect_x = (curr_max_x + curr_min_x) / 2
+        # ehpi_img[1, :, :] = reflect_y - (ehpi_img[1, :, :] - reflect_y)
+        ehpi_img[0, :, :] = reflect_x - (ehpi_img[0, :, :] - reflect_x)
+
+        if bool(random.getrandbits(1)):
+            # Swap Left / Right joints
+            for left_index, right_index in zip(self.left_indexes, self.right_indexes):
+                tmp = np.copy(ehpi_img)
+                ehpi_img[0:, :, left_index] = tmp[0:, :, right_index]
+                ehpi_img[1:, :, left_index] = tmp[1:, :, right_index]
+                ehpi_img[2:, :, left_index] = tmp[2:, :, right_index]
+                ehpi_img[0:, :, right_index] = tmp[0:, :, left_index]
+                ehpi_img[1:, :, right_index] = tmp[1:, :, left_index]
+                ehpi_img[2:, :, right_index] = tmp[2:, :, left_index]
         sample['x'] = ehpi_img
         return sample
