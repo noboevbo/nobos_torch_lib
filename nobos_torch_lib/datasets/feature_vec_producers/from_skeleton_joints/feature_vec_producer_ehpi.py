dfec706526018e31804939678228510bf0a8238a
Dennis Ludl <Dennis.Ludl@reutlingen-university.de>
2019-02-19 18:23:32 +0100

    Added jhmdb joints to ehpi producer

--- nobos_torch_lib/datasets/feature_vec_producers/from_skeleton_joints/feature_vec_producer_ehpi.py
+++ nobos_torch_lib/datasets/feature_vec_producers/from_skeleton_joints/feature_vec_producer_ehpi.py
@@ -5,34 +5,51 @@
 # TODO: (hip, lhand, rhand), (neck, leye, reye)
 # TODO: How to represend a spatial pose (n rows)? How To represen temporal pose (n columns)?
 # TODO: Try to normalize everything by the humans height
-from typing import List
+from typing import Callable
 
 import numpy as np
 from nobos_commons.data_structures.dimension import ImageSize
-from nobos_commons.data_structures.skeletons.joint_2d import Joint2D
 from nobos_commons.data_structures.skeletons.skeleton_stickman import SkeletonStickman
 from nobos_commons.utils.human_surveyor import HumanSurveyor
 
 
 class FeatureVecProducerEhpi(object):
-    def __init__(self, skeleton: SkeletonStickman, image_size: ImageSize, human_surveyor: HumanSurveyor = HumanSurveyor()):
+    def __init__(self, image_size: ImageSize, human_surveyor: HumanSurveyor = HumanSurveyor(),
+                 get_joints_func: Callable = lambda skeleton: FeatureVecProducerEhpi.get_joints_default(skeleton)):
         """
         Creates a feature vector out of 2D pose data like it was proposed in
         Fang et. al (2017) - On-Board Detection of Pedestrian Intentions
         """
-        self.__skeleton_config = skeleton
         self.__image_size = image_size
         self.human_surveyor = human_surveyor
-        joints = self.__get_joints_to_use(skeleton)
+        self.get_joints_func: Callable = get_joints_func
+        joints = self.get_joints_func(SkeletonStickman())
         self.num_joints = len(joints)
         # We have 4 features for each combination of 2 joints and 3 features for each combination of 3 joints
         self.feature_vec_length = int(3 * self.num_joints)
 
-    def __get_joints_to_use(self, skeleton: SkeletonStickman) -> List[Joint2D]:
+    def get_feature_vec(self, skeleton: SkeletonStickman) -> np.ndarray:
+        """
+        Returns the (unnormalized) feature vec
+        :param skeleton:
+        :return:
+        """
+        # human_height = self.human_surveyor.get_human_height(skeleton.limbs)
+        joints = self.get_joints_func(skeleton)
+        feature_vec = np.zeros((len(joints), 3), dtype=np.float32)
+        for idx, joint in enumerate(joints):
+            feature_vec[idx][0] = joint.x / self.__image_size.width
+            feature_vec[idx][1] = joint.y / self.__image_size.height
+            feature_vec[idx][2] = joint.score
+
+        return feature_vec
+
+    @staticmethod
+    def get_joints_default(skeleton: SkeletonStickman):
         return [
             # Head region
             skeleton.joints.nose,
-            skeleton.joints.left_eye,
+            skeleton.joints.neck,
             skeleton.joints.right_eye,
 
             # Torso
@@ -61,18 +78,35 @@ class FeatureVecProducerEhpi(object):
             skeleton.joints.right_ankle,
         ]
 
-    def get_feature_vec(self, skeleton: SkeletonStickman) -> np.ndarray:
+    @staticmethod
+    def get_joints_jhmdb(skeleton: SkeletonStickman):
         """
-        Returns the (unnormalized) feature vec
-        :param skeleton:
-        :return:
+        JHMDB -> No annotated eyes / ears
         """
-        human_height = self.human_surveyor.get_human_height(skeleton.limbs)
-        joints = self.__get_joints_to_use(skeleton)
-        feature_vec = np.zeros((len(joints), 3), dtype=np.float32)
-        for idx, joint in enumerate(joints):
-            feature_vec[idx][0] = joint.x / self.__image_size.width
-            feature_vec[idx][1] = joint.y / self.__image_size.height
-            feature_vec[idx][2] = joint.score
+        return [
+            # Head region
+            skeleton.joints.nose,
+            skeleton.joints.neck,
+            skeleton.joints.hip_center,
 
-        return feature_vec
+            # Left shoulder
+            skeleton.joints.left_shoulder,
+            skeleton.joints.left_elbow,
+            skeleton.joints.left_wrist,
+
+            # Right shoulder
+            skeleton.joints.right_shoulder,
+            skeleton.joints.right_elbow,
+            skeleton.joints.right_wrist,
+
+            # Left leg
+            skeleton.joints.left_hip,
+            skeleton.joints.left_knee,
+            skeleton.joints.left_ankle,
+
+            # Right leg
+            skeleton.joints.right_hip,
+            skeleton.joints.right_knee,
+            skeleton.joints.right_ankle,
+        ]
+
